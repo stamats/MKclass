006c8f277854ebc631363c6e75fb0cb2059a6020
stamats <Matthias.Kohl@stamats.de>
2020-10-03 20:52:42 +0200

    added print and predict method for decisionStump

--- R/decisionStump.R
+++ R/decisionStump.R
@@ -8,12 +8,34 @@ decisionStump <- function(pred, truth, namePos, perfMeasure = "YJS",
                       delta = delta, ...)
   if(!is.factor(truth)) truth <- factor(truth)
   nameNeg <- levels(truth)[levels(truth) != namePos]
-  model <- function(x){
-    factor(ifelse(x > optcut[1], namePos, nameNeg))
-  }
+  model <- function(x){}
+  OC <- optcut[1]
+  names(OC) <- NULL
+  body(model) <- substitute({factor(ifelse(x > OC, namePos, nameNeg))}, 
+                            list(OC = OC,
+                                 namePos = as.character(namePos),
+                                 nameNeg = nameNeg))
   res <- list(call = cl, cutoff = optcut[1], perfMeasure = optcut[2],
               model = model, fitted = model(pred), 
               data = data.frame(pred, truth))
   class(res) <- "decisionStump"
   res
+}
+print.decisionStump <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
+  cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"), 
+      "\n\n", sep = "")
+  cat("Cut-off:\n")
+  print.default(format(x$cutoff, digits = digits), print.gap = 2L, 
+                quote = FALSE)
+  cat("\nPerformance:\n")
+  print.default(format(x$perfMeasure, digits = digits), print.gap = 2L, 
+                quote = FALSE)
+  invisible(x)
+}
+predict.decisionStump <- function(object, newdata, ...){
+  if(missing(newdata) || is.null(newdata))
+    predictor <- object$fitted
+  else
+    predictor <- object$model(newdata)
+  predictor
 }
